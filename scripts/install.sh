a0a0811eee9502aaabf13d9e1e98067ab3e96692
AndrewDaws <AndrewDaws@users.noreply.github.com>
2019-06-20 17:16:59 -0400

    Updated install script. - Removed unused tools from all install groups. - Generate gitconfig based on template. - Added prompts to further refine what applications and configurations are wanted. - Added proper TODOs to better keep track of future tasks.

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,23 +15,30 @@
 #
 
 echo '------------------------------------------------------------------------'
-echo '=> Download user-config repo'
+echo '   Prepare user-config repo'
 echo '------------------------------------------------------------------------'
 
+echo '=> Making .config directory'
 mkdir -p ~/.config
+
+echo '=> Installing git'
 sudo apt install -y --no-install-recommends git
-cd .config
+
+echo '=> Cloning user-config repo'
+cd ~/.config
 git clone git@github.com:AndrewDaws/user-configs.git
 
+echo 'Done.'
+
+
+
+
+
 echo '------------------------------------------------------------------------'
-echo '=> Unix post-install script'
+echo '   Initial configuration'
 echo '------------------------------------------------------------------------'
 
 cd ~/.config/user-configs
-
-# -----------------------------------------------------------------------------
-# => System update/upgrade
-# -----------------------------------------------------------------------------
 
 echo '=> Update repository information'
 sudo apt update -qq
@@ -46,52 +53,43 @@ echo 'Done.'
 
 
 
-# -----------------------------------------------------------------------------
-# => Configure default system configurations and applications
-# -----------------------------------------------------------------------------
+
+
+echo '------------------------------------------------------------------------'
+echo '   Configuring system applications'
+echo '------------------------------------------------------------------------'
 
 #echo '=> Adding repositories'
-curl -sL https://deb.nodesource.com/setup_11.x | sudo bash -
-#sudo add-apt-repository -y ppa:ehoover/compholio          # Netflix
 
 echo '=> Installing system applications'
-sudo apt install -y --no-install-recommends neovim \
-    ruby ruby-dev mosh openssh-server \
-    python3-dev python3-pip tmux zsh htop man \
-    rclone curl nano gawk httpie iputils-ping wget fuse openvpn \
-    cron ack-grep nmap knockd iptables iptables-persistent \
-    make gcc screen meld build-essential cmake \
-    silversearcher-ag nodejs tmuxinator
+sudo apt install -y --no-install-recommends \
+    vim zsh htop man curl nano gawk nmap tmux \
+    openssh-server python3-dev python3-pip \
+    cron httpie iputils-ping \
 sudo pip3 install thefuck
 sudo pip3 install tldr
-sudo pip3 install ntfy
-sudo gem install gist
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 echo '=> Installing system shell'
+# TODO: Fix oh-my-zsh install via sub-shell.
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k
 
-echo '=> Installing system configurations'
+echo '=> Installing system application configurations'
 chmod 644 ~/.config/user-configs/zsh/.zshrc
 chmod 644 ~/.config/user-configs/aliases/.alias
-chmod 644 ~/.config/user-configs/alacritty/alacritty.yml
 chmod 644 ~/.config/user-configs/tmux/.tmux.conf
 rm -f ~/.zshrc
-rm -f ~/.config/alacritty/alacritty.yml
 rm -f ~/.tmux.conf
 rm -f ~/.fzf.bash
 rm -f ~/.fzf.zsh
-//rm -f ~/.gitconfig
 ln -s ~/.config/user-configs/zsh/.zshrc ~/.zshrc
-ln -s ~/.config/user-configs/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s ~/.config/user-configs/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/.config/user-configs/fzf/.fzf.zsh ~/.fzf.zsh
-//ln -s ~/.config/user-configs/git/.gitconfig ~/.gitconfig
 rm -f ~/.bash_history
 rm -f ~/.bash_logout
 rm -f ~/.bashrc
@@ -101,58 +99,103 @@ mkdir -p ~/.config/project-configs
 touch ~/.config/project-configs/.default
 sudo usermod -s "$(command -v zsh)" "${USER}"
 
-echo 'Done.'
-
-
-ln -s ~/.config/user-configs/.gitconfig ~/.gitconfig
-
-# -----------------------------------------------------------------------------
-# => Install desktop applications
-# -----------------------------------------------------------------------------
-
-echo '=> Install desktop applications?'
-echo -e '=> Are you sure? [Y/n] '
-read confirmation
-confirmation=$(echo $confirmation | tr '[:lower:]' '[:upper:]')
-if [[ $confirmation == 'YES' || $confirmation == 'Y' ]]; then
+echo -e '=> Install build tools? [Y/N] '
+read buildConfirm
+buildConfirm=$(echo $buildConfirm | tr '[:lower:]' '[:upper:]')
+if [[ $buildConfirm == 'YES' || $buildConfirm == 'Y' ]]; then
 
     echo '=> Installing desktop applications'
     sudo apt install -y --no-install-recommends \
-        vlc wireshark darktable snapd fonts-powerline \
-        terminator
+        make gcc build-essential cmake snapd
+fi
 
-    #wget https://go.microsoft.com/fwlink/?LinkID=760868
-    #sudo dpkg -i code_*.deb
-    #rm -f code_*.deb
+echo 'Done.'
 
-    wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
-    sudo dpkg -i steam.deb
-    rm -f steam.deb
+
+
+
+
+echo '------------------------------------------------------------------------'
+echo '   Configuring desktop applications'
+echo '------------------------------------------------------------------------'
+
+echo -e '=> Install desktop applications? [Y/N] '
+read desktopConfirm
+desktopConfirm=$(echo $desktopConfirm | tr '[:lower:]' '[:upper:]')
+if [[ $desktopConfirm == 'YES' || $desktopConfirm == 'Y' ]]; then
+
+    #echo '=> Installing desktop applications'
+    # TODO: Automate alacritty, chrome-stable, and double commander install via apt or static download link.
+    #sudo apt install -y --no-install-recommends \
+    #    alacritty
 
     echo '=> Installing desktop fonts'
-    git clone https://github.com/powerline/fonts.git --depth=1; ./fonts/install.sh; rm -rf fonts;
     git clone https://github.com/ryanoasis/nerd-fonts.git --depth 1; ./nerd-fonts/install.sh; rm -rf nerd-fonts;
     sudo fc-cache -f -v
 
     echo '=> Installing desktop configurations'
-    rm -f ~/.config/terminator/config
-    ln -s ~/.config/user-configs/config ~/.config/terminator/config
+    chmod 644 ~/.config/user-configs/alacritty/alacritty.yml
+    rm -f ~/.config/alacritty/alacritty.yml
+    ln -s ~/.config/user-configs/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-    echo 'Done.'
+    echo -e '=> Install development applications? [Y/N] '
+    read developmentConfirm
+    developmentConfirm=$(echo $developmentConfirm | tr '[:lower:]' '[:upper:]')
+    if [[ $developmentConfirm == 'YES' || $developmentConfirm == 'Y' ]]; then
+
+        echo '=> Installing development applications'
+        # TODO: Automate beyond compare install via apt or static download link.
+        sudo apt install -y --no-install-recommends \
+            wireshark meld
+        
+        # TODO: Fix Visual Studio Code install with correct static download link.
+        #wget https://go.microsoft.com/fwlink/?LinkID=760868
+        #sudo dpkg -i code_*.deb
+        #rm -f code_*.deb
+
+        rm -f ~/.gitconfig
+        cp ~/.config/user-configs/git/.gitconfig ~/.gitconfig
+        echo "" >> ~/.gitconfig
+        echo "[user]" >> ~/.gitconfig
+        echo 'What is your Git name?'
+        read gitName
+        echo "    name = $gitName" >> ~/.gitconfig
+        echo 'What is your Git email?'
+        read gitEmail
+        echo "    email = $gitEmail" >> ~/.gitconfig
+    fi
+
+    echo -e '=> Install gaming applications? [Y/N] '
+    read gamingConfirm
+    gamingConfirm=$(echo $gamingConfirm | tr '[:lower:]' '[:upper:]')
+    if [[ $gamingConfirm == 'YES' || $gamingConfirm == 'Y' ]]; then
+
+        echo '=> Installing gaming applications'
+        wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+        sudo dpkg -i steam.deb
+        rm -f steam.deb
+    fi
+
+    echo -e '=> Install photography applications? [Y/N] '
+    read photographyConfirm
+    photographyConfirm=$(echo $photographyConfirm | tr '[:lower:]' '[:upper:]')
+    if [[ $photographyConfirm == 'YES' || $photographyConfirm == 'Y' ]]; then
+
+        echo '=> Installing photography applications'
+        sudo apt install -y --no-install-recommends \
+            darktable
+    fi
 fi
 
-#todo:
-#double-commander
-#vs code
-#beyond compare
-#wireguard
-#chrome-stable
+echo 'Done.'
 
 
 
-# -----------------------------------------------------------------------------
-# => System update/upgrade
-# -----------------------------------------------------------------------------
+
+
+echo '------------------------------------------------------------------------'
+echo '   System updates and upgrades'
+echo '------------------------------------------------------------------------'
 
 echo '=> Update repository information'
 sudo apt update -qq
