add5f5e8dde1660e82bad25721577064b9c9b9aa
AndrewDaws <AndrewDaws@users.noreply.github.com>
2020-02-17 23:06:25 -0500

    Testing out awk-based file processing

--- scripts/create_gitconfig.sh
+++ scripts/create_gitconfig.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+# Creates or updates gitconfig based off template settings stored in the repo.
+script_name="$(basename "${0}")"
+
+argument_flag="false"
+force_mode="disabled"
+username_passed=""
+email_passed=""
+
+# Process arguments
+for argument in "${@}"
+do
+  argument_flag="true"
+  if [[ "${argument}" == "-?" || "${argument}" == "--help" ]]; then
+    echo "Usage:"
+    echo "  ${script_name} [options]"
+    echo "  -?, --help      show list of command-line options"
+    echo ""
+    echo "OPTIONS"
+    echo "      --force  force create new gitconfig"
+    exit 0
+  elif [[ "${argument}" == "--force" ]]; then
+    force_mode="enabled"
+  else
+    echo "Aborting ${script_name}"
+    echo "  Invalid Argument!"
+    echo ""
+    echo "Usage:"
+    echo "  ${script_name} [options]"
+    echo "  -?, --help      show list of command-line options"
+    exit 1
+  fi
+done
 
 # @todo Preserve Git Config
 # @body Preserve an existing .gitconfig and update with new settings.
