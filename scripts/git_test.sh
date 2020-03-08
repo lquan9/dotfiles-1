8638714565e80c04e7592cf41d1ed0a3de6b4036
AndrewDaws <AndrewDaws@users.noreply.github.com>
2020-03-07 21:44:36 -0500

    Furthur testing git file info extraction.

--- scripts/git_test.sh
+++ scripts/git_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #awk '/\[log\]/{x=NR+2}(NR<=x){print}' "${HOME}/.gitconfig"
-#awk '/\[log\]/{section=1;next}/\[.*\]/{section=0}section' "${HOME}/.gitconfig"
+testvar="$(awk '/\[log\]/{section=1;next}/\[.*\]/{section=0}section' "${HOME}/.gitconfig")"
 #awk '/\[.*\]/{section=1;print;next}/\[.*]/{print;section=0}section' "${HOME}/.gitconfig"
-awk ' BEGIN {  print "The number of times difftools appears in the file is:" ; }
-/difftool/ {  counter+=1  ;  }
-END {  printf "%s\n",  counter  ; } 
-'  "${HOME}/.gitconfig"
+# awk ' BEGIN {  print "The number of times difftools appears in the file is:" ; }
+# /difftool/ {  counter+=1  ;  }
+# END {  printf "%s\n",  counter  ; } 
+# '  "${HOME}/.gitconfig"
+echo "${testvar}"
